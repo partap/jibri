7840103f7e62d4af26800614d5914a48ef332ca0
Partap Davis <partapdavis@gmail.com>
2020-07-31 13:59:33 -0600

    hardcode java 8 for jibri launch

--- resources/debian-package/opt/jitsi/jibri/launch.sh
+++ resources/debian-package/opt/jitsi/jibri/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec java -Djava.util.logging.config.file=/etc/jitsi/jibri/logging.properties -jar /opt/jitsi/jibri/jibri.jar --config "/etc/jitsi/jibri/config.json"
+exec /usr/lib/jvm/adoptopenjdk-8-hotspot-amd64/bin/java -Djava.util.logging.config.file=/etc/jitsi/jibri/logging.properties -jar /opt/jitsi/jibri/jibri.jar --config "/etc/jitsi/jibri/config.json"
